5094053ae206b403c265ef389665e91c1d6e2b69
Justin Kwong <justin.kwong@osi.ca.gov>
2017-08-10 20:26:48 +0000

    justin tool initial commit

---
+++ lib/justin_view_tool.rb
@@ -0,0 +1,6 @@
+require "justin_view_tool/version"
+require "justin_view_tool/renderer"
+
+module JustinViewTool
+  # Your code goes here...
+end
---
+++ lib/justin_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JustinViewTool
+  VERSION = "0.1.0"
+end
